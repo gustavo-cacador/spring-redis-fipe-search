6f740bfe527819b9e5b78ac429b78c5706784533
Gustavo Caçador <gustavoirl456@outlook.com>
2026-04-08 23:27:43 -0300

    projeto criado

---
+++ src/main/java/com/gustavoronchi/fipe_search/FipeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.fipe_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeSearchApplication.class, args);
+	}
+
+}
